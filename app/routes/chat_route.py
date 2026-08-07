ab29b828db8574b7a07ba500b4fd68950fd9ddb1
vinaypanchal007 <vinayp.12601@gmail.com>
2026-08-07 18:35:24 +0530

    File name change

--- app/routes/chat_route.py
+++ app/routes/chat_route.py
@@ -2,13 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# @app.get("/")
-# def home():
-#     return {
-#         "message":"Welcome to AI Document Assistant",
-#         "status":"successfully connected"
-#     }
-    
 @app.post("/chat")
 def upload_file():
     return {
