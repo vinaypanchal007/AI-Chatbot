04c9e19f1e0e63e924f49bc11310c27abdc91961
vinaypanchal007 <vinayp.12601@gmail.com>
2026-08-08 20:05:42 +0530

    edited document_processor.py and pipeline.py

---
+++ app/rag/document_processor.py
@@ -0,0 +1,33 @@
+import io
+
+from fastapi import UploadFile
+from PyPDF2 import PdfReader
+from io import BytesIO
+
+async def process_document(file: UploadFile) -> str:
+    if file.content_type == "text/plain":
+        return await process_text(file)
+    
+    elif file.content_type == "application/pdf":
+        return await process_pdf(file)
+    
+    else:
+        return ValueError("Unsupported file type. Please upload a text or PDF file.")
+    
+
+async def process_text(file: UploadFile) -> str:
+    context = await file.read()
+    return context.decode("utf-8")
+
+async def process_pdf(file: UploadFile) -> str:
+    content = await file.read()
+    pdf = PdfReader(io.BytesIO(content))
+    text = ""
+    
+    for page in pdf.pages:
+        page_text += page.extract_text()
+        
+        if page_text:
+            text += page_text + "\n"
+            
+    return text
--- app/rag/pipeline.py
+++ app/rag/pipeline.py
@@ -1,12 +1,17 @@
+from fastapi import UploadFile
+from app.rag.document_processor import process_document
+
 async def general_chat(message: str):
     return {
         "mode" : "General Chat",
         "response" : f"Received message: {message}"
     }
-    
-async def rag_chat(message: str, file):
+
+async def rag_chat(message: str, file: UploadFile):
+
+    text = await process_document(file)
+
     return {
-        "mode" : "RAG Chat",
-        "filename" : file.filename,
-        "response" : f"Received message: {message}"
+        "message": message,
+        "document_text": text
     }
