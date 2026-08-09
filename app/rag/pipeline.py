050af850c8d760b8f34c4b3d07b30cd03b0b6604
vinaypanchal007 <vinayp.12601@gmail.com>
2026-08-09 14:51:09 +0530

    applying text cleaning to pipeline.py

--- app/rag/pipeline.py
+++ app/rag/pipeline.py
@@ -1,6 +1,7 @@
 from fastapi import UploadFile
 from app.rag.document_processor import process_document
 from app.rag.chunker import chunk_text
+from app.rag.text_cleaner import clean_text
 
 async def general_chat(message: str):
     return {
@@ -10,8 +11,10 @@ async def general_chat(message: str):
 
 async def rag_chat(message: str, file: UploadFile):
     text = await process_document(file)
+    text = clean_text(text)
     chunks = chunk_text(text)
     return {
+        "mode": "RAG",
         "message": message,
         "chunks": chunks
     }
