9c2cfe8947be795668857eec32a8b858263ea0dd
vinaypanchal007 <vinayp.12601@gmail.com>
2026-08-11 21:22:22 +0530

    Edited chunker and text_cleaner

--- app/rag/text_cleaner.py
+++ app/rag/text_cleaner.py
@@ -2,6 +2,6 @@ import re
 
 def clean_text(text: str) -> str:
     text = re.sub(r"[\t]+", " ", text)
-    text = re.sub(r"\n{3, }", "\n\n", text)
+    text = re.sub(r"\n{3,}", "\n\n", text)
     text = text.strip()
     return text
