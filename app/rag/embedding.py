2fef8890f8680111f3eae877a1f71e3bf3cf1dff
vinaypanchal007 <vinayp.12601@gmail.com>
2026-08-11 18:10:20 +0530

    Modified embedding.py

--- app/rag/embedding.py
+++ app/rag/embedding.py
@@ -1,6 +1,7 @@
+import os
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer('all-MiniLM-L6-v2')
+model = SentenceTransformer(os.environ.get("EMBEDDING_MODEL"))
 
 def create_embeddings(chunks: list[str]):
     embeddings = model.encode(chunks)
