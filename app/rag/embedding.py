67500bdbc4000ef0ecb739029b3baef8f72f9d1c
vinaypanchal007 <vinayp.12601@gmail.com>
2026-08-11 19:31:58 +0530

    Edoted embedding.py and retriever.py

--- app/rag/embedding.py
+++ app/rag/embedding.py
@@ -1,7 +1,7 @@
 import os
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer(os.environ.get("EMBEDDING_MODEL"))
+model = SentenceTransformer("all-MiniLM-L6-v2")
 
 def create_embeddings(chunks: list[str]):
     embeddings = model.encode(chunks)
--- app/rag/retriever.py
+++ app/rag/retriever.py
@@ -1,5 +1,5 @@
 from app.rag.embedding import create_embeddings
-from app.rag.vector_db import vector_db
+from app.rag.vector_db import store_embeddings
 
 def retrieve_chunks(query: str, top_k: int= 3):
     
@@ -7,7 +7,7 @@ def retrieve_chunks(query: str, top_k: int= 3):
     query_embedding = create_embeddings([query])
     
     #search FAISS index for the top_k search results
-    _, positions = vector_db.index.search(
+    _, positions = store_embeddings.index.search(
         query_embedding.astype('float32'),
         top_k
     )
@@ -17,6 +17,6 @@ def retrieve_chunks(query: str, top_k: int= 3):
     
     for pos in positions[0]:
         if pos != -1:
-            results.append(vector_db.chunk_store[pos])
+            results.append(store_embeddings.chunk_store[pos])
     
     return results
